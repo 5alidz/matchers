f1b28b7b0885a8925c7d10c62c6ed758b1bd383d
5alidz <kzack-dev@hotmail.com>
2020-03-18 16:01:30 +0300

    implement pipe function

--- pipe.js
+++ pipe.js
@@ -21,17 +21,4 @@ function pipe(initialValue) {
   };
 }
 
-const playerData = pipe({ x: 0, y: 0 })(
-  ({ x, y }) => ({ x: x + 1, y: y + 2 }),
-  [true, ({ x, y }) => ({ x: x - 50, y: y + 100 })],
-  [false, ({ x, y }) => ({ x, y }), ({ x, y }) => ({ x: x + 150, y: y - 20 })],
-  ({ x, y }) => ({ x, y, score: x + y })
-);
-
-const addOne = val => val + 1;
-const half = val => val / 2;
-const sqr = val => val * val;
-
-const myCalc = pipe(0)(addOne, half, sqr);
-
-console.log(playerData, myCalc);
+module.exports = pipe;
